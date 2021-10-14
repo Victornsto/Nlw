45e4d7b839dd722cb0b7d2f1e75d9c89930ff368
Victornsto <victornsto@gmail.com>
2021-10-13 22:57:25 -0300

    Adiciona efeitos e correções
    Adiciona efeito icones do footer
    Adiciona icone back-to-back e script para sumir com scroll
    Organiza assets

--- main.js
+++ main.js
@@ -67,3 +67,13 @@ scrollReveal.reveal(
     interval: 100
   }
 )
+
+// Bottom back to top
+const backToTopButton = document.querySelector('.back-to-top')
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+})
